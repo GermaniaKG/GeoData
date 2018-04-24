981b3391554a8e62b7159a8ac85d95b4bb87de3b
Webdev Walter <412560+tomkyle@users.noreply.github.com>
2018-04-24 13:36:12 +0200

    Prepare for multiple types

--- src/NotEmptyGeoDataFilterIterator.php
+++ src/NotEmptyGeoDataFilterIterator.php
@@ -32,12 +32,12 @@ class NotEmptyGeoDataFilterIterator extends \FilterIterator
         $item = $this->getInnerIterator()->current();
 
         // Disclose items not implementing GeoDataProviderInterface
-        if (!$item instanceOf GeoDataProviderInterface) {
-            return false;
+        if ($item instanceOf GeoDataProviderInterface) {
+            $status = (!empty($item->getLatitude()) and !empty($item->getLongitude()));
+            return $status == $this->not_empty_status;
         }
 
-        $status = (!empty($item->getLatitude()) and !empty($item->getLongitude()));
+        return false;
 
-        return $status == $this->not_empty_status;
     }
 }
