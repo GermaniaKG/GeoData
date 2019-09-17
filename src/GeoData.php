4cba9310344d875ec4e25d64187299055d78415a
Webdev Walter <412560+tomkyle@users.noreply.github.com>
2019-09-17 17:04:21 +0200

    Make params optional

--- src/GeoData.php
+++ src/GeoData.php
@@ -9,7 +9,7 @@ class GeoData extends GeoDataAbstract implements GeoDataInterface, GeoDataProvid
      * @param float $latitude
      * @param float $longitude
      */
-    public function __construct( float $latitude, float $longitude)
+    public function __construct( ?float $latitude, ?float $longitude)
     {
         $this->latitude  = $latitude;
         $this->longitude = $longitude;
