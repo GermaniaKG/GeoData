1fa946b409bd5796f52ccab69cf89f75457ea623
Webdev Walter <412560+tomkyle@users.noreply.github.com>
2022-06-28 08:42:36 +0200

    Smaller fixes

--- src/GeoDataAwareInterface.php
+++ src/GeoDataAwareInterface.php
@@ -8,5 +8,5 @@ interface GeoDataAwareInterface extends GeoDataProviderInterface
      * @param mixed GeoDataProviderInterface|GeoDataInterface
      * @return self
      */
-    public function setGeoData($geodata) : self;
+    public function setGeoData($geodata);
 }
